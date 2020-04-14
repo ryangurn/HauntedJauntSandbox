93b80971e34c2a8ab3be1854becb25a8c3fc23bd
Ryan Gurnick <ryangurnick@gmail.com>
2020-04-13 19:24:55 -0700

    Guidance Disappearing Act
    Changed the way guidance shows when a game ending is triggered.

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -5,65 +5,67 @@ using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
-    public float fadeDuration = 1f;
-    public float displayImageDuration = 1f;
-    public GameObject player;
-    public CanvasGroup exitBackgroundImageCanvasGroup;
-    public AudioSource exitAudio;
-    public CanvasGroup caughtBackgroundImageCanvasGroup;
-    public AudioSource caughtAudio;
+	public float fadeDuration = 1f;
+	public float displayImageDuration = 1f;
+	public GameObject player;
+	public GameObject guidance;
+	public CanvasGroup exitBackgroundImageCanvasGroup;
+	public AudioSource exitAudio;
+	public CanvasGroup caughtBackgroundImageCanvasGroup;
+	public AudioSource caughtAudio;
 
-    bool m_IsPlayerAtExit;
-    bool m_IsPlayerCaught;
-    float m_Timer;
-    bool m_HasAudioPlayed;
-    
-    void OnTriggerEnter (Collider other)
-    {
-        if (other.gameObject == player)
-        {
-            m_IsPlayerAtExit = true;
-        }
-    }
+	bool m_IsPlayerAtExit;
+	bool m_IsPlayerCaught;
+	float m_Timer;
+	bool m_HasAudioPlayed;
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == player)
+		{
+			m_IsPlayerAtExit = true;
+		}
+	}
 
-    public void CaughtPlayer ()
-    {
-        m_IsPlayerCaught = true;
-    }
+	public void CaughtPlayer ()
+	{
+		m_IsPlayerCaught = true;
+	}
 
-    void Update ()
-    {
-        if (m_IsPlayerAtExit)
-        {
-            EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
-        }
-        else if (m_IsPlayerCaught)
-        {
-            EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
-        }
-    }
+	void Update ()
+	{
+		if (m_IsPlayerAtExit)
+		{
+			EndLevel (exitBackgroundImageCanvasGroup, false, exitAudio);
+		}
+		else if (m_IsPlayerCaught)
+		{
+			EndLevel (caughtBackgroundImageCanvasGroup, true, caughtAudio);
+		}
+	}
 
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
-    {
-        if (!m_HasAudioPlayed)
-        {
-            audioSource.Play();
-            m_HasAudioPlayed = true;
-        }
-            
-        m_Timer += Time.deltaTime;
-        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+	void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+	{
+		guidance.SetActive(false);
+		if (!m_HasAudioPlayed)
+		{
+			audioSource.Play();
+			m_HasAudioPlayed = true;
+		}
+			
+		m_Timer += Time.deltaTime;
+		imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-        if (m_Timer > fadeDuration + displayImageDuration)
-        {
-            if (doRestart)
-            {
-                SceneManager.LoadScene (0);
-            }
-            else
-            {
-                Application.Quit ();
-            }
-        }
-    }
+		if (m_Timer > fadeDuration + displayImageDuration)
+		{
+			if (doRestart)
+			{
+				SceneManager.LoadScene (0);
+			}
+			else
+			{
+				Application.Quit ();
+			}
+		}
+	}
 }
