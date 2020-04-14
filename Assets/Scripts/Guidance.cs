a83dc9faae545f89a2da478217bec276a506cbb8
Ryan Gurnick <ryangurnick@gmail.com>
2020-04-13 19:44:58 -0700

    Adding Interpolating Colors
    This will change the colors of the guidance based on the temperature of the players movement (red=hot, purpleish=warm, blue=cold). This utilized color lerp which aims to make the math simple for a vector4 color (RGBA)

--- Assets/Scripts/Guidance.cs
+++ Assets/Scripts/Guidance.cs
@@ -6,63 +6,68 @@ using UnityEngine.UI;
 public class Guidance : MonoBehaviour
 {
 
-    public GameObject exitBox;
-    public GameObject player;
+	public GameObject exitBox;
+	public GameObject player;
 
-    Text m_text;
+	Text m_text;
+	Color c = Color.white;
+	float m_exitX, m_exitZ;
 
-    float m_exitX, m_exitZ;
+	void Start()
+	{
+		m_text = GetComponent<Text>();
+		m_exitX = exitBox.transform.position[0];
+		m_exitZ = exitBox.transform.position[2];
+	}
 
-    void Start()
-    {
-        m_text = GetComponent<Text>();
-        m_exitX = exitBox.transform.position[0];
-        m_exitZ = exitBox.transform.position[2];
-    }
+	void Update()
+	{
+		string distanceToExit;
+		int distance;
+		float playerRotAngleRad, playerRotAngleDeg;
+		float playerX = player.transform.position[0];
+		float playerZ = player.transform.position[2];
+		Vector2 playerRotNormalizedXZ = new Vector2();
+		Vector2 playerToExitNormalizedXZ = new Vector2();
+		float playerLook, angleToExit;
 
-    void Update()
-    {
-        string distanceToExit;
-        int distance;
-        float playerRotAngleRad, playerRotAngleDeg;
-        float playerX = player.transform.position[0];
-        float playerZ = player.transform.position[2];
-        Vector2 playerRotNormalizedXZ = new Vector2();
-        Vector2 playerToExitNormalizedXZ = new Vector2();
-        float playerLook, angleToExit;
+		
+		distance = (int) Mathf.Sqrt(Mathf.Pow(m_exitX - playerX, 2) + Mathf.Pow(m_exitZ - playerZ, 2));
+		distanceToExit = "Distance to exit: " + distance;
 
-        
-        distance = (int) Mathf.Sqrt(Mathf.Pow(m_exitX - playerX, 2) + Mathf.Pow(m_exitZ - playerZ, 2));
-        distanceToExit = "Distance to exit: " + distance;
+		playerRotAngleDeg = player.transform.rotation.eulerAngles[1];
+		playerRotAngleRad = playerRotAngleDeg * Mathf.Deg2Rad;
 
-        playerRotAngleDeg = player.transform.rotation.eulerAngles[1];
-        playerRotAngleRad = playerRotAngleDeg * Mathf.Deg2Rad;
+		playerRotNormalizedXZ[0] = Mathf.Sin(playerRotAngleRad);
+		playerRotNormalizedXZ[1] = Mathf.Cos(playerRotAngleRad);
 
-        playerRotNormalizedXZ[0] = Mathf.Sin(playerRotAngleRad);
-        playerRotNormalizedXZ[1] = Mathf.Cos(playerRotAngleRad);
-
-        playerToExitNormalizedXZ[0] = m_exitX - playerX;
-        playerToExitNormalizedXZ[1] = m_exitZ - playerZ;
-        playerToExitNormalizedXZ.Normalize();
-
-
-        playerLook = Mathf.Acos(playerRotNormalizedXZ[0] * playerToExitNormalizedXZ[0] + playerRotNormalizedXZ[1] * playerToExitNormalizedXZ[1]);
+		playerToExitNormalizedXZ[0] = m_exitX - playerX;
+		playerToExitNormalizedXZ[1] = m_exitZ - playerZ;
+		playerToExitNormalizedXZ.Normalize();
 
 
-        angleToExit = (playerLook * Mathf.Rad2Deg);
+		playerLook = Mathf.Acos(playerRotNormalizedXZ[0] * playerToExitNormalizedXZ[0] + playerRotNormalizedXZ[1] * playerToExitNormalizedXZ[1]);
 
-        if (angleToExit <= 60)
-        {
-            m_text.text = distanceToExit + "\n" + "Hot";
-        }
-        else if (angleToExit <= 120)
-        {
-            m_text.text = distanceToExit + "\n" + "Warmer";
-        }
-        else
-        {
-            m_text.text = distanceToExit + "\n" + "Cold";
-        }
 
-    }
+		angleToExit = (playerLook * Mathf.Rad2Deg);
+
+		if (angleToExit <= 60)
+		{
+			m_text.text = distanceToExit + "\n" + "Hot";
+			c = Color.Lerp(Color.blue, Color.red, 1.0f);
+		}
+		else if (angleToExit <= 120)
+		{
+			m_text.text = distanceToExit + "\n" + "Warmer";
+			c = Color.Lerp(Color.blue, Color.red, 0.65f);
+		}
+		else
+		{
+			m_text.text = distanceToExit + "\n" + "Cold";
+			c = Color.Lerp(Color.blue, Color.red, 0.0f);
+		}
+
+		m_text.color = c;
+
+	}
 }
